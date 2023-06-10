f0663cf98df5377fcb9cfadd7ed09d6411ae10e3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-06-10 16:57:11 +0100

    Add geo_point

--- doc/concepts/geo_point/rails-generate.sh
+++ doc/concepts/geo_point/rails-generate.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 set -euf
+
+rails generate scaffold \
+edge \
+    $RAILS_GENERATE_SCAFFOLD_FIELDS_BEFORE \
+    latitude:"decimal{15,10}":index \
+    longitude:"decimal{15,10}":index \
+    altitude_msl:integer:index \
+    altitude_agl:meters:index \
+    elevation_msl:integer:index \
+    elevation_agl:integer:index \
+    $RAILS_GENERATE_SCAFFOLD_FIELDS_AFTER
