5f053af63b423977905c825a5b9587e3858fec75
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-06-12 17:33:06 +0100

    Add plan_okr

---
+++ doc/concepts/plan_okr/rails-generate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -euf
+
+rails generate scaffold 
+plan_okr \
+	$RAILS_GENERATE_SCAFFOLD_FIELDS_BEFORE \
+    objective_id:references:index \
+    key_result_0_id:references:index \
+    key_result_1_id:references:index \
+    key_result_2_id:references:index \
+    $RAILS_GENERATE_SCAFFOLD_FIELDS_AFTER
