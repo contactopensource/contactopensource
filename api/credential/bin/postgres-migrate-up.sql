331e02b193d9bc4ce2194e694b8a4c1624a1c578
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-12-28 22:14:01 -0800

    Add api/credential/

---
+++ api/credential/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE credential (
+);
+
+CREATE INDEX index_credential_id on credential(id);
+CREATE INDEX index_credential_zid on credential(zid);
+CREATE INDEX index_credential_title on credential(title);
+CREATE INDEX index_credential_subtitle on credential(subtitle);
+
