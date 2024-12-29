eeb3853befa815fdb2113ef9e87c622ff0ae1b67
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-12-28 22:33:02 -0800

    Add api/trait/

---
+++ api/trait/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS trait;
+DROP INDEX IF EXISTS index_trait_id;
+DROP INDEX IF EXISTS index_trait_zid;
+DROP INDEX IF EXISTS index_trait_title;
+DROP INDEX IF EXISTS index_trait_subtitle;
+
---
+++ api/trait/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE trait (
+);
+
+CREATE INDEX index_trait_id on trait(id);
+CREATE INDEX index_trait_zid on trait(zid);
+CREATE INDEX index_trait_title on trait(title);
+CREATE INDEX index_trait_subtitle on trait(subtitle);
+
