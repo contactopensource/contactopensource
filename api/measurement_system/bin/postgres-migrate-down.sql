daf9ddafdf48d7a5f6845789bcd97440a0a1fff7
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-12-29 16:41:36 -0800

    Add api/measurement_system/

---
+++ api/measurement_system/bin/postgres-migrate-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS measurement_system;
+DROP INDEX IF EXISTS index_measurement_system_id;
+DROP INDEX IF EXISTS index_measurement_system_zid;
+DROP INDEX IF EXISTS index_measurement_system_title;
+DROP INDEX IF EXISTS index_measurement_system_subtitle;
+DROP INDEX IF EXISTS index_measurement_system_key;
+
---
+++ api/measurement_system/bin/postgres-migrate-up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE measurement_system (
+);
+
+CREATE INDEX index_measurement_system_id on measurement_system(id);
+CREATE INDEX index_measurement_system_zid on measurement_system(zid);
+CREATE INDEX index_measurement_system_title on measurement_system(title);
+CREATE INDEX index_measurement_system_subtitle on measurement_system(subtitle);
+CREATE INDEX index_measurement_system_key on measurement_system(key);
+
