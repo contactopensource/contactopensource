8aad70dfc4648d2856176c4d4e47c1194772b1fb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-12-30 05:25:02 -0800

    Add api/node/

---
+++ api/node/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS node;
+DROP INDEX IF EXISTS index_node_id;
+DROP INDEX IF EXISTS index_node_zid;
+DROP INDEX IF EXISTS index_node_title;
+DROP INDEX IF EXISTS index_node_subtitle;
+
---
+++ api/node/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE node (
+);
+
+CREATE INDEX index_node_id on node(id);
+CREATE INDEX index_node_zid on node(zid);
+CREATE INDEX index_node_title on node(title);
+CREATE INDEX index_node_subtitle on node(subtitle);
+
