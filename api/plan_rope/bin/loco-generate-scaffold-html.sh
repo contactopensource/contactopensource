f70a37a33603517071f30de26000ea7c45214c78
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-04 13:34:28 -0800

    Rename from rope_estimate into plan_rope

---
+++ api/plan_rope/bin/loco-generate-scaffold-html.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -euf
+
+cargo loco generate scaffold --html \
+    plan_rope \
+        id:bigint \
+        zid:uuid \
+        lock_version:int \
+        created_at:timestamp \
+        created_by:url \
+        updated_at:timestamp \
+        updated_by:url \
+        retired_at:timestamp \
+        retired_by:url \
+        locale_id:references \
+        title:"string{50}" \
+        subtitle:"string{50}" \
+        summary:string \
+        description:text \
+        disambiguation:text \
+        image_1024x1024_url:url \
+        image_1024x1024_alt:text \
+        emoji:text \
+        star_count:integer \
+        measurement_unit_id:references \
+        realistic:card.id \
+        optimistic:card.id \
+        pessimistic:card.id \
+        equilibristic:card.id \
+
---
+++ api/plan_rope/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plan_rope (
+);
+
+CREATE INDEX index_plan_rope_id on plan_rope(id);
+CREATE INDEX index_plan_rope_zid on plan_rope(zid);
+CREATE INDEX index_plan_rope_title on plan_rope(title);
+CREATE INDEX index_plan_rope_subtitle on plan_rope(subtitle);
+
