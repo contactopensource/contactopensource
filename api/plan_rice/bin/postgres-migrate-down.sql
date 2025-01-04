ead98a9d0e8bb109180b7c55fd515c5b88696123
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-04 13:27:57 -0800

    Add api/plan_rice/

---
+++ api/plan_rice/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS plan_rice;
+DROP INDEX IF EXISTS index_plan_rice_id;
+DROP INDEX IF EXISTS index_plan_rice_zid;
+DROP INDEX IF EXISTS index_plan_rice_title;
+DROP INDEX IF EXISTS index_plan_rice_subtitle;
+
---
+++ api/plan_rice/bin/rails-generate-scaffold.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -euf
+
+cargo loco generate scaffold \
+    plan_rice \
+        id:bigint:index \
+        zid:uuid:index \
+        lock_version:intNone \
+        created_at:timestampNone \
+        created_by:urlNone \
+        updated_at:timestampNone \
+        updated_by:urlNone \
+        retired_at:timestampNone \
+        retired_by:urlNone \
+        locale_id:locale.idNone \
+        title:"string{50}":index \
+        subtitle:"string{50}":index \
+        summary:stringNone \
+        description:textNone \
+        disambiguation:textNone \
+        image_1024x1024_url:urlNone \
+        image_1024x1024_alt:textNone \
+        emoji:textNone \
+        star_count:integerNone \
+        reach:card.idNone \
+        impact:card.idNone \
+        confidence:card.idNone \
+        effort_id:card.idNone \
+    --force \
+    --no-timestamps \
+
