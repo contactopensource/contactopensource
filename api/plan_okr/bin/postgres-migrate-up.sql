50296a25f589f8a2314f4d0c1dea70d1d3be71a9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-04 14:18:14 -0800

    Add api/plan_okr/

---
+++ api/plan_okr/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plan_okr (
+);
+
+CREATE INDEX index_plan_okr_id on plan_okr(id);
+CREATE INDEX index_plan_okr_zid on plan_okr(zid);
+CREATE INDEX index_plan_okr_title on plan_okr(title);
+CREATE INDEX index_plan_okr_subtitle on plan_okr(subtitle);
+
