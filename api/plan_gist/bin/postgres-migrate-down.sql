99b6e52204b67ea111f8cc94c421ea495959afd9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-04 13:31:40 -0800

    Add api/plan_gist

---
+++ api/plan_gist/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS plan_gist;
+DROP INDEX IF EXISTS index_plan_gist_id;
+DROP INDEX IF EXISTS index_plan_gist_zid;
+DROP INDEX IF EXISTS index_plan_gist_title;
+DROP INDEX IF EXISTS index_plan_gist_subtitle;
+
