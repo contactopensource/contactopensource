ae7e8c381e6d612183290daf07c02c60a0182c34
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-05 07:39:49 -0800

    Add api/geographic_area/

---
+++ api/geographic_area/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE geographic_area (
+);
+
+CREATE INDEX index_geographic_area_id on geographic_area(id);
+CREATE INDEX index_geographic_area_zid on geographic_area(zid);
+CREATE INDEX index_geographic_area_title on geographic_area(title);
+CREATE INDEX index_geographic_area_subtitle on geographic_area(subtitle);
+
