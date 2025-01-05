ba1ef961a69049b2f4b5006ffc6ca1895208ec37
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-05 07:39:58 -0800

    Add api/item/

---
+++ api/item/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE item (
+);
+
+CREATE INDEX index_item_id on item(id);
+CREATE INDEX index_item_zid on item(zid);
+CREATE INDEX index_item_title on item(title);
+CREATE INDEX index_item_subtitle on item(subtitle);
+
---
+++ api/item/bin/rails-generate-scaffold.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -euf
+
+cargo loco generate scaffold \
+    item \
+        id:bigint:index \
+        zid:uuid:index \
+        lock_version:intNone \
+        created_at:timestampNone \
+        created_by:urlNone \
+        updated_at:timestampNone \
+        updated_by:urlNone \
+        retired_at:timestampNone \
+        retired_by:urlNone \
+        locale_id:locale.idNone \
+        title:"string{50}":index \
+        subtitle:"string{50}":index \
+        summary:stringNone \
+        description:textNone \
+        disambiguation:textNone \
+        image_1024x1024_url:urlNone \
+        image_1024x1024_alt:textNone \
+        emoji:textNone \
+        star_count:integerNone \
+        geographic_point_id:geographic_point.idNone \
+        color_point_rgb_with_unit_interval_id:color_point_rgb_with_unit_interval.idNone \
+        quick_response_code_id:quick_response_code.idNone \
+        universal_product_code_id:universal_product_code.idNone \
+    --force \
+    --no-timestamps \
+
