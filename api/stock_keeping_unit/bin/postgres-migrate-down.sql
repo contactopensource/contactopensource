e2aad1fb4a02126cb3b5ffec57af3a7282a81385
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-05 07:40:20 -0800

    Add api/stock_keeping_unit/

---
+++ api/stock_keeping_unit/bin/postgres-migrate-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS stock_keeping_unit;
+DROP INDEX IF EXISTS index_stock_keeping_unit_id;
+DROP INDEX IF EXISTS index_stock_keeping_unit_zid;
+DROP INDEX IF EXISTS index_stock_keeping_unit_title;
+DROP INDEX IF EXISTS index_stock_keeping_unit_subtitle;
+DROP INDEX IF EXISTS index_stock_keeping_unit_code;
+
---
+++ api/stock_keeping_unit/bin/postgres-migrate-up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE stock_keeping_unit (
+);
+
+CREATE INDEX index_stock_keeping_unit_id on stock_keeping_unit(id);
+CREATE INDEX index_stock_keeping_unit_zid on stock_keeping_unit(zid);
+CREATE INDEX index_stock_keeping_unit_title on stock_keeping_unit(title);
+CREATE INDEX index_stock_keeping_unit_subtitle on stock_keeping_unit(subtitle);
+CREATE INDEX index_stock_keeping_unit_code on stock_keeping_unit(code);
+
