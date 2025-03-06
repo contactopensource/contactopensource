4c02490b9397eff4e2441eced5b5a36e825437b9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-06 08:31:26 +0000

    Add religion

---
+++ api/religion/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS religion;
+DROP INDEX IF EXISTS index_religion_id;
+DROP INDEX IF EXISTS index_religion_zid;
+DROP INDEX IF EXISTS index_religion_title;
+DROP INDEX IF EXISTS index_religion_subtitle;
+
