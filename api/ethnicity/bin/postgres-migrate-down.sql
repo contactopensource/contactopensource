59ac8b4650501dabcc5391d18aef311e15741d74
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-06 08:31:00 +0000

    Add ethnicity

---
+++ api/ethnicity/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS ethnicity;
+DROP INDEX IF EXISTS index_ethnicity_id;
+DROP INDEX IF EXISTS index_ethnicity_zid;
+DROP INDEX IF EXISTS index_ethnicity_title;
+DROP INDEX IF EXISTS index_ethnicity_subtitle;
+
---
+++ api/ethnicity/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ethnicity (
+);
+
+CREATE INDEX index_ethnicity_id on ethnicity(id);
+CREATE INDEX index_ethnicity_zid on ethnicity(zid);
+CREATE INDEX index_ethnicity_title on ethnicity(title);
+CREATE INDEX index_ethnicity_subtitle on ethnicity(subtitle);
+
