b342dbd09108e80a7f5d0e7e6bd6d9a50398a526
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-07 15:27:59 +0000

    Add disability

---
+++ api/disability/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS disability;
+DROP INDEX IF EXISTS index_disability_id;
+DROP INDEX IF EXISTS index_disability_zid;
+DROP INDEX IF EXISTS index_disability_title;
+DROP INDEX IF EXISTS index_disability_subtitle;
+
---
+++ api/disability/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE disability (
+);
+
+CREATE INDEX index_disability_id on disability(id);
+CREATE INDEX index_disability_zid on disability(zid);
+CREATE INDEX index_disability_title on disability(title);
+CREATE INDEX index_disability_subtitle on disability(subtitle);
+
