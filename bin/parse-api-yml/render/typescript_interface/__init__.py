b693b98ea0b4a24bb880df29ad6c6b4c09925805
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-13 09:45:14 +0000

    Add help

--- bin/parse-api-yml/render/typescript_interface/__init__.py
+++ bin/parse-api-yml/render/typescript_interface/__init__.py
@@ -9,6 +9,22 @@ interface Person {
     age: number;
 };
 ```
+
+Usage:
+
+```typescript
+let p: Person = {
+    name: "Alice", 
+    age: 21
+};
+
+function output(p: Person) {
+    console.log(p.name);
+    console.log(p.age);
+}
+
+output(p);
+```
 """
 
 from pathlib import Path
