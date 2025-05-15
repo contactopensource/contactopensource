8ec812bd8f9597facde78c6728c2006c8648641d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-15 18:28:43 +0100

    Add api/quality

---
+++ api/quality/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE quality (
+);
+
+CREATE INDEX index_quality_id on quality(id);
+CREATE INDEX index_quality_zid on quality(zid);
+CREATE INDEX index_quality_title on quality(title);
+CREATE INDEX index_quality_subtitle on quality(subtitle);
+
