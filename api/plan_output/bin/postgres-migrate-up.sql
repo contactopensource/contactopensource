65dbc1b46eef9a4af9048456bd0c6862179a436f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-15 18:28:27 +0100

    Add api/plan_output

---
+++ api/plan_output/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plan_output (
+);
+
+CREATE INDEX index_plan_output_id on plan_output(id);
+CREATE INDEX index_plan_output_zid on plan_output(zid);
+CREATE INDEX index_plan_output_title on plan_output(title);
+CREATE INDEX index_plan_output_subtitle on plan_output(subtitle);
+
