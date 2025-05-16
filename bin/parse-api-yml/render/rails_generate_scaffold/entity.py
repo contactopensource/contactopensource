26b0a59a3aa356a46337e234fd77786a44cd9567
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-16 19:42:49 +0100

    Add associations

--- bin/parse-api-yml/render/rails_generate_scaffold/entity.py
+++ bin/parse-api-yml/render/rails_generate_scaffold/entity.py
@@ -1,16 +1,11 @@
 from . import attribute_group
 
 def render(entity):
-    if entity is None:
-        print("entity is None")
-        return None
-    else:
-        print(f"HERE rails_generate_scaffold render entity: {entity}")
-        s = f"rails generate scaffold \\\n"
-        s += f"    {entity.id} \\\n"
-        if entity.attribute_groups is not None:
-            s += ''.join(map(attribute_group.render, entity.attribute_groups))
-        s += "    --force \\\n"
-        s += "    --no-timestamps \\\n"
-        s += "\n"
-        return s
+    s = f"rails generate scaffold \\\n"
+    s += f"    {entity.id} \\\n"
+    if entity.attribute_groups is not None:
+        s += ''.join(map(attribute_group.render, entity.attribute_groups))
+    s += "    --force \\\n"
+    s += "    --no-timestamps \\\n"
+    s += "\n"
+    return s
