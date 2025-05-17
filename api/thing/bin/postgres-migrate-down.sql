85154cb9fbf53662a9f5e0d1f5072589f4be5a13
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-17 12:59:53 +0100

    Add api/thing

---
+++ api/thing/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS thing;
+DROP INDEX IF EXISTS index_thing_id;
+DROP INDEX IF EXISTS index_thing_zid;
+DROP INDEX IF EXISTS index_thing_title;
+DROP INDEX IF EXISTS index_thing_subtitle;
+
---
+++ api/thing/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE thing (
+);
+
+CREATE INDEX index_thing_id on thing(id);
+CREATE INDEX index_thing_zid on thing(zid);
+CREATE INDEX index_thing_title on thing(title);
+CREATE INDEX index_thing_subtitle on thing(subtitle);
+
