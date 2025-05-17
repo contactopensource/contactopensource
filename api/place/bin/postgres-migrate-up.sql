565738b9206fd3a27eb6c163b640ee7a720d0f0d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-17 12:59:30 +0100

    Add api/place

---
+++ api/place/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE place (
+);
+
+CREATE INDEX index_place_id on place(id);
+CREATE INDEX index_place_zid on place(zid);
+CREATE INDEX index_place_title on place(title);
+CREATE INDEX index_place_subtitle on place(subtitle);
+
