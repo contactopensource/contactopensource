970c7d2fadbe8dd568a963a00bf1136b6a44631f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-17 13:00:05 +0100

    Add api/plan_budget

---
+++ api/plan_budget/bin/postgres-migrate-down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS plan_budget;
+DROP INDEX IF EXISTS index_plan_budget_id;
+DROP INDEX IF EXISTS index_plan_budget_zid;
+DROP INDEX IF EXISTS index_plan_budget_title;
+DROP INDEX IF EXISTS index_plan_budget_subtitle;
+
---
+++ api/plan_budget/bin/postgres-migrate-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plan_budget (
+);
+
+CREATE INDEX index_plan_budget_id on plan_budget(id);
+CREATE INDEX index_plan_budget_zid on plan_budget(zid);
+CREATE INDEX index_plan_budget_title on plan_budget(title);
+CREATE INDEX index_plan_budget_subtitle on plan_budget(subtitle);
+
