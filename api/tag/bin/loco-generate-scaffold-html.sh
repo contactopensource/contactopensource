46e1ed23fc2da3b092d5af5585aa326bd5cc76b8
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-05-18 20:21:17 +0100

    Add api/tag

---
+++ api/tag/bin/loco-generate-scaffold-html.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -euf
+
+cargo loco generate scaffold --html \
+    tag \
+        id:uuid \
+        lock_version:integer \
+        created_at:timestamp \
+        created_by:url \
+        updated_at:timestamp \
+        updated_by:url \
+        retired_at:timestamp \
+        retired_by:url \
+        locale_id:references \
+        title:"string{50}" \
+        subtitle:"string{50}" \
+        summary:"string{255}" \
+        description:text \
+        disambiguation:text \
+        image_1024x1024_url:url \
+        image_1024x1024_alt:text \
+        emoji:text \
+        star_count:integer \
+        quick_response_code_id:references \
+        universal_product_code_id:references \
+        key:string \
+        description:text \
+
---
+++ api/tag/bin/postgres-migrate-up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tag (
+);
+
+CREATE INDEX index_tag_id on tag(id);
+CREATE INDEX index_tag_title on tag(title);
+CREATE INDEX index_tag_subtitle on tag(subtitle);
+
