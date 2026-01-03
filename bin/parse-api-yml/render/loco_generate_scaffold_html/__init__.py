add14a15b2ae4e0624b8254dc8b2733bcd0fefc0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2026-01-03 14:10:09 +0000

    Add file parent mkdir

--- bin/parse-api-yml/render/loco_generate_scaffold_html/__init__.py
+++ bin/parse-api-yml/render/loco_generate_scaffold_html/__init__.py
@@ -6,11 +6,14 @@ The generator generates a controller with view, templates, schema and context fo
 
 from functools import partial
 from pathlib import Path
-from . import entity
+
 from ..helpers import sh
+from . import entity
+
 
 def output_path():
-    return Path("bin", "loco-generate-scaffold-html.sh")
+    return Path("bin", "loco", "loco-generate-scaffold-html.sh")
+
 
 def render(args, entities):
-    return sh() + ''.join(map(partial(entity.render, args), entities))
+    return sh() + "".join(map(partial(entity.render, args), entities))
